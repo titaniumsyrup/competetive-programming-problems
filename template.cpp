0ba9a0960ef4d946f7b8c07a067e826513c077a5
pranav <rajpranav424@gmail.com>
2024-06-05 00:40:46 +0530

    1200

---
+++ template.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <iomanip>
+#include <map>
+#include <deque>
+#include <queue>
+#include <utility>
+using namespace std;
+typedef long long ll;
+ 
+void solve() {
+    ll n;
+    cin >> n;
+    ll arr[n];
+    
+    for (ll i = 0; i < n; i++) {
+        cin>>arr[i];
+    }
+
+}
+int main() {
+    ll t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
